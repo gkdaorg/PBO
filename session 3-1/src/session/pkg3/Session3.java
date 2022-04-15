cb2717f8bbce65ff68e720bfc9847f7d2ea0fac9
milson <97680938+gkdaorg@users.noreply.github.com>
2022-04-15 09:48:43 +0700

    Add files via upload

---
+++ session 3-1/src/session/pkg3/Session3.java
@@ -0,0 +1,23 @@
+
+package session.pkg3;
+public class Session3 {
+    public static void main(String[] args) {
+       { String x = "manudia";
+        System.out.println("saya adalah " + x );
+        System.out.println(x + "adalah sebuah mahluk hidup");
+        System.out.println(x+ "ini terkadang baik terkadangjahat juga ya  ");
+        System.out.println( x + " ini penuh dengan kehausan sesuatu yang tidak bisa "
+        + "dipuaskan ");
+       }
+       String y = "kitty";
+        System.out.println(y);
+        System.out.println(y);
+        
+       y = "halo";
+        System.out.println(y);
+        
+       
+   }
+       
+    
+}
