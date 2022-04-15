348a25f8c3ea6213f9abd28bf04dab0c54f17d01
milson <97680938+gkdaorg@users.noreply.github.com>
2022-04-15 09:50:34 +0700

    Add files via upload

---
+++ numberic data 3-2/src/numberic/data/pkg3/pkg2/NumbericData32.java
@@ -0,0 +1,20 @@
+package numberic.data.pkg3.pkg2;
+public class NumbericData32 {
+    public static void main(String[] args) {
+     {int x = 30;
+    x = 35;
+    x = 2+ 5; 
+    System.out.println(x); }
+     
+{ int x = 20 ;  
+     x = 25;
+     x= 2+5;
+     x = x+1;
+     x++;
+     System.out.println(x);
+ } 
+
+  
+    }
+    
+}
