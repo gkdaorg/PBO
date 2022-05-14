9430370a6420340f0a1feed2249da8cf36b7c9cc
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 17:04:56 +0700

    Add files via upload

---
+++ nanti/src/nanti/Nanti.java
@@ -0,0 +1,18 @@
+package nanti;
+public class Nanti {
+   public static void main(String[] args) {
+       {String greetings = " ello ";
+           System.out.println(greetings );
+        }
+       {
+     String var1 = " ini baju baju";
+     String var2= " ini ini celana celana";
+           System.out.println(var1 + var2);
+       }
+       {
+           string totalprice = "total: rp-"
+       }
+       
+}}
+    
+
