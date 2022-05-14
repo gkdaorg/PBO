d407b4ea815368dc3a1bb0add1469049796e2848
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 16:52:32 +0700

    Add files via upload

---
+++ contructor/src/contructor/Contructor.java
@@ -0,0 +1,15 @@
+
+package contructor;
+
+public class Contructor {
+
+    public static void main(String[] args) {
+   
+        String test = null;
+        System.out.println(test.length());
+       
+        
+        
+    }
+    
+}
