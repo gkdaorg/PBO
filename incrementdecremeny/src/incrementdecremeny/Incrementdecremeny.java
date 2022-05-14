e5a8907e2b0ebfa43504ff3dbc5bc798082bbe72
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 11:54:10 +0700

    Add files via upload

---
+++ incrementdecremeny/src/incrementdecremeny/Incrementdecremeny.java
@@ -0,0 +1,17 @@
+package incrementdecremeny;
+public class Incrementdecremeny {
+    public static void main(String[] args) {
+   int count = 30;
+   int a, b ,c, d;
+   a = count ++;
+   b= count ;
+   c = ++count;
+   d = count; 
+     System.out.println(a+","+ b + ", " + c + ", "+ d);
+        
+        
+        
+        
+    }
+    
+}
