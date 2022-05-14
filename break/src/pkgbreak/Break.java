abd13866a7985cc3c7f1aa3fc6ccf9a2aed97457
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 16:51:14 +0700

    Add files via upload

---
+++ break/src/pkgbreak/Break.java
@@ -0,0 +1,26 @@
+package pkgbreak;
+
+public class Break {
+
+    public static void main(String[] args) {
+     
+     int i =0;
+     while ( i < 10 ){
+         System.out.println(i +"\t");
+         i++;
+         if (i==4){
+             break;
+  }
+}
+    }
+
+       }
+
+    
+
+     
+        
+        
+    
+    
+
