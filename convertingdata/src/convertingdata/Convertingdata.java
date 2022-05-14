17c92fd4f14ca4e9664da17e46858dc096082a92
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 11:50:30 +0700

    Add files via upload

---
+++ convertingdata/src/convertingdata/Convertingdata.java
@@ -0,0 +1,22 @@
+package convertingdata;
+public class Convertingdata {
+    public static void main(String[] args) {
+        { int num1= 7 ;
+      double num2 = 2;
+      double num3;
+      num3= num1 / num2;
+    }
+        {int myInt;
+    double mypercent= 51.9;
+    myInt = (int)mypercent;
+        }
+        {int myInt;
+    double mylong= 99L;
+    myInt = (int)mylong;}
+        
+        
+        
+        
+    }
+    
+}
