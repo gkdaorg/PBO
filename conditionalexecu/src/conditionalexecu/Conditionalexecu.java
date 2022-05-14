83d4aee9a79cee55c075dcdfb2060182b6fc9200
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 16:52:10 +0700

    Add files via upload

---
+++ conditionalexecu/src/conditionalexecu/Conditionalexecu.java
@@ -0,0 +1,25 @@
+package conditionalexecu;
+public class Conditionalexecu {
+    public static void main(String[] args) {
+    int day=0;
+    int nilai=95;
+    if (nilai >=88 && day == 0){
+        System.out.println("kamu dapat schoolarship");
+    }
+    else{
+        System.out.println("kamu tidak qualifide" + "shoolarship");
+    }
+    
+    }
+}
+    
+    
+    
+        
+        
+        
+        
+        
+    
+    
+
