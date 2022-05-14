e39efe18c192bfcef0229b29c8eb258e2decceb1
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 17:04:25 +0700

    Add files via upload

---
+++ loops/src/loops/Loops.java
@@ -0,0 +1,27 @@
+package loops;
+public class Loops {
+    public static void main(String[] args) {
+        {  
+        System.out.println("terbang");
+        for (int i = 10; i >= 10; i--){
+        System.out.println(i);
+        }
+        System.out.println(" gooooo");
+        }
+        {  
+      for(int i = 1; i <=5; i++){
+          System.out.println("i squared = " + i *i );}
+        }
+        
+        
+        
+    }
+}
+        
+        
+        
+        
+        
+    }
+    
+}
