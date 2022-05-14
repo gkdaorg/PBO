1b70abec6c16b6cbceeb0eb37af6cee7dbef3994
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 17:05:57 +0700

    Add files via upload

---
+++ tugas/src/tugas/Tugas.java
@@ -0,0 +1,16 @@
+package tugas;
+public class Tugas {
+public static void main(String[] args) {
+    
+    System.out.println( "//// selamat datang di kedai bijelek ///" );
+    System.out.println("  silahkan pilih yang tersedia:   "); 
+     
+     
+     
+     
+     
+    
+      
+    }
+    
+}
