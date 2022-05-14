4133d46fc23159c30679ab1b281f54795b728ed5
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 17:06:55 +0700

    Add files via upload

---
+++ textual daya/src/textual/daya/TextualDaya.java
@@ -0,0 +1,22 @@
+package textual.daya;
+public class TextualDaya {
+    public static void main(String[] args) {
+        {String totalprice = "total: $ "+3 ;
+        System.out.println(totalprice);
+        
+      String harga = "ini : $"+3+4+5; 
+        System.out.println(harga);
+        
+      String persen = "ini progreessnya hanya %"+50;
+        System.out.println(persen);
+        }
+        
+        System.out.println("the cat said \"Meow!\" to me."  );
+        
+    
+    
+        
+        
+    }
+    
+}
