793e84f249c15aecba97e6be9c2e1e71da40f303
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 17:06:24 +0700

    Add files via upload

---
+++ while and dowhile/src/pkgwhile/and/dowhile/WhileAndDowhile.java
@@ -0,0 +1,19 @@
+package pkgwhile.and.dowhile;
+public class WhileAndDowhile {
+    public static void main(String[] args) {
+        {    
+int i= 10;
+    System.out.println("launch ");
+    while (i >=0){
+        System.out.println(i);
+        i--;
+    }
+    System.out.println("tebanggggg");
+}
+}
+}
+
+
+        
+ 
+    
