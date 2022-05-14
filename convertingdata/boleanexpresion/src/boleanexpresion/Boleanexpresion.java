40d8504d2397d9a194d5a5742157868ed8c90be3
milson <97680938+gkdaorg@users.noreply.github.com>
2022-05-14 17:02:37 +0700

    Add files via upload

---
+++ convertingdata/boleanexpresion/src/boleanexpresion/Boleanexpresion.java
@@ -0,0 +1,25 @@
+package boleanexpresion;
+public class Boleanexpresion {
+    public static void main(String[] args) {
+     
+        {boolean res1 = 24  == 15;
+        System.out.println("res1: " + res1);
+        int value1 = 15;
+        int value2 = 24;
+        boolean res2 = value1 == value2;
+        System.out.println("res2: " + res2);
+            System.out.println("===================================");
+    }
+        { int x = 3;
+        int y = 2;
+        int z = x+y;
+        boolean test = (z==x+y);
+        System.out.println(z);
+        System.out.println(test);  
+        }       
+        
+        
+        
+    }
+    
+}
